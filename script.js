d372e01fd4487d644d4225a573bae5daf687239e
Marco-Sorrentino <sorrentmarco@gmail.com>
2022-12-24 17:43:48 +0100

    finished
    to fix only just a detail on span line

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const container = document.querySelector(".container");
 const containerThanks = document.querySelector(".container-thanks");
 const submitButton = document.getElementById("submit");
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".cicle");
 
 submitButton.addEventListener("click", () => {
   containerThanks.classList.remove("hidden");
+  container.style.display = "none";
+});
+
+rates.forEach((rate) => {
+  rate.addEventListener("click", () => {
+    rating.innerHTML = rate.innerHTML;
+  });
 });

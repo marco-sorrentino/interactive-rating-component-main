080b4b1087ad5dd39cf3bd7ae5e6ef50a587ea01
Marco-Sorrentino <sorrentmarco@gmail.com>
2022-12-24 17:54:21 +0100

    added rate again function

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ const containerThanks = document.querySelector(".container-thanks");
 const submitButton = document.getElementById("submit");
 const rating = document.getElementById("rating");
 const rates = document.querySelectorAll(".cicle");
+const returnback = document.querySelector(".return");
 
 submitButton.addEventListener("click", () => {
   containerThanks.classList.remove("hidden");
   container.style.display = "none";
+});
+
+returnback.addEventListener("click", () => {
+  containerThanks.classList.add("hidden");
+  container.style.display = "block";
 });
 
 rates.forEach((conta) => {
